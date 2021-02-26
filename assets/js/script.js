9b813c385f0f4119d6f96495a168c9128ea8f904
JPB <jbanas9124@gmail.com>
2021-02-26 16:57:23 -0600

    functional task name/type inputs

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,22 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(){
   event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  //create list item in DOM
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+  //Create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  //give the div a class name
+  taskInfoEl.className = "task-info";
+  //add all necessary HTML content to div
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  //listItemEl.textContent = taskNameInput; removed now that taskInfoEl is holding this information
+  
+  //add taskInfoEl to listItemEl
+  listItemEl.appendChild(taskInfoEl);
+  //add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
